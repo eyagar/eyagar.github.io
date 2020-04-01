22ad2257b4e9f8f964cd8a1ed8a6cd6390364d9b
eyagar <50244985+eyagar@users.noreply.github.com>
2020-04-01 13:22:32 -0400

    update on js

--- asst_2/main.js
+++ asst_2/main.js
@@ -5,6 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for(let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(clickit) {
+      displayedImage.src = clickit.target.src;
+    }
+  }
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
