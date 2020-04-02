4d4c3c7b291305aeb6c7fdcb455ca7a802bfaf59
tylerbehr <60366856+tylerbehr@users.noreply.github.com>
2020-04-02 13:19:37 -0400

    add name

--- asst_2/main.js
+++ asst_2/main.js
@@ -1,7 +1,7 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
+const button = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
@@ -9,8 +9,8 @@ for(let i = 1; i <= 5; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', 'images/pic' + i + '.jpg');
     thumbBar.appendChild(newImage);
-    newImage.onclick = function(clickit) {
-      displayedImage.src = clickit.target.src;
+    newImage.onclick = function(clickIt) {
+      displayedImage.src = clickIt.target.src;
     }
   }
 
@@ -19,3 +19,15 @@ newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+button.onclick = function(){
+  const buttonClass = button.getAttribute('class');
+  if(buttonClass === 'dark'){
+    button.setAttribute('class', 'light');
+    button.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    button.setAttribute('class', 'dark');
+    button.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
